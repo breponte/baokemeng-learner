18d25c9f209b05a7b5c2e5d0dcf9f6aa404d07ef
breponte <bkreponte@ucsd.edu>
2026-09-05 18:52:01 -0500

    feat: containerize tts demo

--- tts/tts.cpp
+++ tts/tts.cpp
@@ -1,7 +1,8 @@
 #include <cstdint>
 #include <cstdio>
 #include <string>
-
+#include <fstream>
+#include <iostream>
 #include "sherpa-onnx/c-api/cxx-api.h"
 
 static int32_t ProgressCallback(const float *samples, int32_t num_samples,
@@ -24,8 +25,22 @@ int32_t main(int32_t argc, char *argv[]) {
   config.model.debug = 0;
   config.rule_fsts = "model/vits-piper-zh_CN-chaowen-medium/phone.fst,model/vits-piper-zh_CN-chaowen-medium/date.fst,model/vits-piper-zh_CN-chaowen-medium/number.fst";
 
-  std::string filename = "./test.wav";
-  std::string text = "某某银行的副行长和一些行政领导表示,他们去过长江和长白山; 经济不断增长。2024年12月31号,拨打110或者18920240511。123456块钱。当夜幕降临,星光点点,伴随着微风拂面,我在静谧中感受着时光的流转,思念如涟漪荡漾,梦境如画卷展开,我与自然融为一体,沉静在这片宁静的美丽之中,感受着生命的奇迹与温柔.";
+  std::string filename = "./output/output.wav";
+
+  std::ifstream input_file("./input/input.txt");
+  // Check if the file was opened successfully
+  if (!input_file.is_open()) {
+      std::cerr << "Error opening the file!";
+      return 1;
+  }
+
+  std::string text;
+
+  // Read 1st line from the file
+  std::getline(input_file, text);
+
+  // Close the file
+  input_file.close();
 
   auto tts = OfflineTts::Create(config);
 
